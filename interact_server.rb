7ef6381a87484c7df8f0784c234b0e0c8127c846
Luis Mario Urrea <mario.urrea@codescrum.com>
2015-10-28 09:24:16 -0500

    solved the first challenge

---
+++ interact_server.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+
+class InteractServer
+
+  def talk_to_the_server(url)
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    res.body
+  end
+
+  # get a word to the server
+  def get_word(word_url)
+    talk_to_the_server(URI.parse(word_url))
+  end
+
+  # send a word to he server
+  def send_word(answer_url, word)
+    talk_to_the_server(URI.parse(answer_url+word))
+  end
+
+end
+
---
+++ response_to_the_server.rb
@@ -0,0 +1,24 @@
+require './interact_server.rb'
+
+resource_name = "http://localhost:4567/"
+word_path = "word"
+answer_path = "answer?answer="
+
+puts "This program is connect to the server: #{resource_name}, the String Encoder Game Server (by Codescrum)."
+puts "\n"
+puts "You ask the server for a word, thereby:"
+word_url = resource_name+word_path
+puts "Use this url = #{word_url}"
+puts "\n"
+answer_url = resource_name+answer_path
+puts "You can solve the challenge employed this url = #{answer_url}<put_the_decoded_word_here>"
+
+puts "\n"
+
+objet_interaction = InteractServer.new
+
+word = objet_interaction.get_word(word_url)
+
+puts "Challenge by decoded the word = #{word}"
+
+puts "Is solved?: #{objet_interaction.send_word(answer_url, word)}"
