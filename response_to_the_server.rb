d2ae3c0718384ab1d95e65fde209402f3972d3c5
Luis Mario Urrea <mario.urrea@codescrum.com>
2015-10-28 10:00:22 -0500

    solved the fourth challenge

--- response_to_the_server.rb
+++ response_to_the_server.rb
@@ -17,12 +17,14 @@ puts "\n"
 
 objet_interaction = InteractServer.new
 
+# 1
 word = objet_interaction.get_word(word_url)
 
 puts "Challenge one: by decoded the word = #{word}"
 
 puts "Is solved?: #{objet_interaction.send_word(answer_url, word)}"
 
+# 2
 word = objet_interaction.get_word(word_url)
 
 puts "Challenge two: by decoded the word = #{word}"
@@ -31,6 +33,7 @@ decoding_word = word.reverse
 
 puts "Is solved?: #{objet_interaction.send_word(answer_url, decoding_word)}"
 
+# 3
 word = objet_interaction.get_word(word_url)
 
 puts "Challenge three: by decoded the word = #{word}"
@@ -38,3 +41,23 @@ puts "Challenge three: by decoded the word = #{word}"
 decoding_word = word.gsub(/[13406]/, '1' => 'i', '3' => 'e', '4' => 'a', '0' => 'o', '6' => 'u')
 
 puts "Is solved?: #{objet_interaction.send_word(answer_url, decoding_word)}"
+
+# 4
+word = objet_interaction.get_word(word_url)
+
+puts "Challenge fourth: by decoded the word = #{word}"
+
+word_to_char = word.chars
+
+word_length = word.length
+
+a = word_to_char[word_length-3..word_length]
+
+b = word_to_char[0..word_length-4]
+
+z = a << b
+
+decoding_word = z.join
+
+puts "Is solved?: #{objet_interaction.send_word(answer_url, decoding_word)}"
+
