5a5e199dd3febbd3f49516319a71717d4123659f
Luis Mario Urrea <mario.urrea@codescrum.com>
2015-10-28 19:17:54 -0500

    refactor with case statement to challenge 4

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'activesupport', '~> 3.0'
+gem 'pry'
--- response_to_the_server.rb
+++ response_to_the_server.rb
@@ -7,57 +7,57 @@ answer_path = "answer?answer="
 puts "This program is connect to the server: #{resource_name}, the String Encoder Game Server (by Codescrum)."
 puts "\n"
 puts "You ask the server for a word, thereby:"
-word_url = resource_name+word_path
-puts "Use this url = #{word_url}"
+url_to_get_word = resource_name+word_path
+puts "Use this url = #{url_to_get_word}"
 puts "\n"
-answer_url = resource_name+answer_path
-puts "You can solve the challenge employed this url = #{answer_url}<put_the_decoded_word_here>"
-
+url_to_send_answer = resource_name+answer_path
+puts "You can solve the challenge employed this url = #{url_to_send_answer}<put_the_decoded_word_here>"
 puts "\n"
 
-objet_interaction = InteractServer.new
+interactor = InteractServer.new
 
-# 1
-word = objet_interaction.get_word(word_url)
+challenges = [1,2,3,4]
 
-puts "Challenge one: by decoded the word = #{word}"
+challenges.each do |challenge|
+  puts "The challenge #{challenge} is:"
+  word = interactor.call_to_the_url(url_to_get_word)
+  case challenge
+  when 1
+    decoded_word = word
+  when 2
+    decoded_word = word.reverse
+  when 3
+    decoded_word = word.gsub(/[13406]/, '1' => 'i', '3' => 'e', '4' => 'a', '0' => 'o', '6' => 'u')
+    # binding.pry
+  when 4
+    word_to_char = word.chars
+    word_length = word.length
+    a = word_to_char[word_length-3..word_length]
+    b = word_to_char[0..word_length-4]
+    z = a << b
+    decoded_word = z.join
+  when 5
+    # create this code
+    # number_of_asterisks = word.count('*')
+    # a = 'aeiou'.chars
 
-puts "Is solved?: #{objet_interaction.send_word(answer_url, word)}"
+    # word.sub('*', a.sample(1).join).sub('*', a.sample(1).join).sub('*', a.sample(1).join).sub('*', a.sample(1).join).sub('*', a.sample(1).join)
+  else
+    puts "no challenge configured"
+  end
+  puts "#{word} => #{decoded_word}"
+  response = interactor.call_to_the_url(url_to_send_answer, decoded_word)
+  puts "#{response}"
+end
 
-# 2
-word = objet_interaction.get_word(word_url)
 
-puts "Challenge two: by decoded the word = #{word}"
 
-decoding_word = word.reverse
 
-puts "Is solved?: #{objet_interaction.send_word(answer_url, decoding_word)}"
 
-# 3
-word = objet_interaction.get_word(word_url)
 
-puts "Challenge three: by decoded the word = #{word}"
 
-decoding_word = word.gsub(/[13406]/, '1' => 'i', '3' => 'e', '4' => 'a', '0' => 'o', '6' => 'u')
 
-puts "Is solved?: #{objet_interaction.send_word(answer_url, decoding_word)}"
 
-# 4
-word = objet_interaction.get_word(word_url)
 
-puts "Challenge fourth: by decoded the word = #{word}"
 
-word_to_char = word.chars
-
-word_length = word.length
-
-a = word_to_char[word_length-3..word_length]
-
-b = word_to_char[0..word_length-4]
-
-z = a << b
-
-decoding_word = z.join
-
-puts "Is solved?: #{objet_interaction.send_word(answer_url, decoding_word)}"
 
